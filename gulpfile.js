d94f5113d81d4429973e239a1aa0513e0847cd47
Sanjay Shrestha <dziner.sanjay@gmail.com>
2017-05-29 16:35:04 +0545

    files updated.

--- gulpfile.js
+++ gulpfile.js
@@ -113,7 +113,7 @@ gulp.task('sass', function() {
 				port: 3001,
 				rootPath: outputPath,
 				overviewPath: 'README.md',
-				appRoot: '/output',
+				appRoot: '/pv-styleguide/output/',
 			}))
 			.pipe(gulp.dest(outputPath))
 		});
